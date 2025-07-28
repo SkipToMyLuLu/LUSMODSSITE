c0fca938d8da4cee1a82d780544cb6556e26e5b0
SkipToMyLuLu <skip2mylulu@icloud.com>
2025-07-28 14:58:01 -0400

    Added thing to store querys in session data

--- ModsSite.js
+++ ModsSite.js
@@ -2,9 +2,9 @@ var aquireHtmlInput = document.getElementById('<div contenteditable="plaintext-o
 
 var search_querys_ORGIN = aquireHtmlInput
 console.log('search querys original:   ' + search_querys_ORGIN)
-var SQ_tobe_ARAY = search_querys_ORGIN.toLowerCase();
-console.log('search querys to search for BEOFRE SPLITTING   ' + SQ_tobe_ARAY)
-var search_querysARAY = SQ_tobe_ARAY.split(' ');
+// var SQ_tobe_ARAY = search_querys_ORGIN.toLowerCase();
+// console.log('search querys to search for BEOFRE SPLITTING   ' + SQ_tobe_ARAY)
+var search_querysARAY = search_querys_ORGIN.replaceAll(' ', ',').split(',');
 console.log('search querys to search for AFTER SPLITTING   ' + search_querysARAY)
 
 const search_querys_list_container = document.getElementById('SEARCH-QUERYS-LIST');
@@ -16,4 +16,14 @@ search_querysARAY.forEach(item => {
     ulElement.appendChild(liElement);
 })
 
-{/* <div contenteditable="plaintext-only"></div> */ }
+var querys_stringified = search_querysARAY.join('');
+
+// Saving data
+sessionStorage.setItem('SEARCH-INPUTS', search_querysARAY);
+
+// Retrieving data
+const savedSessionData = sessionStorage.getItem('SEARCH-INPUTS');
+if (savedSessionData) {
+    // Apply savedSessionData to your HTML element
+    document.getElementById('SEARCH-QUERYS-LIST').value = savedSessionData;
+}
